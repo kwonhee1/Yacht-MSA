0859f2ce64979345ebf92b98c79707aa54c63cfa
kwonhee1 <lkh8033@gmail.com>
2026-03-19 18:50:56 +0900

    refactor (User.PasswordEncoder): refactor add passwordencoder salt
    refactor (PasswordEncoder): add salt

--- User/src/main/java/HooYah/User/passwordencoder/BCryptPasswordEncoder.java
+++ User/src/main/java/HooYah/User/passwordencoder/BCryptPasswordEncoder.java
@@ -4,12 +4,27 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class BCryptPasswordEncoder implements PasswordEncoder {
 
+    private final int salt;
+
+    /*
+        @Param : the salt to hash with (perhaps generated using BCrypt.gensalt)
+     */
+    public BCryptPasswordEncoder(
+            int salt
+    ) {
+        this.salt = salt;
+    }
+
+    public BCryptPasswordEncoder() {
+        this(10);
+    }
+
     @Override
     public String encode(CharSequence rawPassword) {
         if (rawPassword == null) {
             throw new IllegalArgumentException("rawPassword cannot be null");
         }
-        return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt());
+        return BCrypt.hashpw(rawPassword.toString(), BCrypt.gensalt(salt));
     }
 
     @Override
